2bbcb3294d2e79449727c441c46636f303f1141c
HangYongmao <1981217256@qq.com>
2018-01-31 17:47:30 +0800

    Compelete

--- app/src/main/java/com/example/a360floatwindowdemo/MainActivity.java
+++ app/src/main/java/com/example/a360floatwindowdemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.a360floatwindowdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startFloatWindow = (Button) findViewById(R.id.start_float_window);
+        startFloatWindow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, FloatWindowService.class);
+                startService(intent);
+                finish();
+            }
+        });
     }
 }
